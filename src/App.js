99c0c634dce19f32e6b2143c675fc4ba40f9a279
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-17 22:18:31 -0300

    deleteBook func and update on createBook func

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { useState } from "react";
 import BookCreate from "../src/components/BookCreate";
+import BookList from "./components/BookList";
 
 const App = () => {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log(title);
+    if (!title) {
+      return;
+    }
+    setBooks([...books, { id: Math.round(Math.random() * 9999), title }]);
   };
 
+  const deleteBookById = (id) => {
+    // filter gives us a brand new array
+    const updatedBooks = books.filter((book) => book.id !== id);
+    setBooks(updatedBooks);
+  };
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
