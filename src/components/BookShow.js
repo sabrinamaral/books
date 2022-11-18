7e25d11d0eab6b1ae62f28254df397b9dd47d291
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-17 22:17:37 -0300

    BookShow component

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,7 +1,17 @@
-import React from "react";
-
-const BookShow = () => {
-  return <div>BookShow</div>;
+const BookShow = ({ book, onDelete }) => {
+  const handleClick = () => {
+    onDelete(book.id);
+  };
+  return (
+    <div>
+      <div className="actions">
+        <button className="delete" onClick={handleClick}>
+          delete
+        </button>
+      </div>
+      <div className="book-show">{book.title}</div>
+    </div>
+  );
 };
 
 export default BookShow;
