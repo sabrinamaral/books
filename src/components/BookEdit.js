17f200910efb6588c45e1e959b0aeac1a6d8e8b4
sabrinamaral <sabrinamaral@hotmail.com>
2022-11-18 15:10:58 -0300

    BookEdit component

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const BookEdit = () => {
-  return <div>BookEdit</div>;
+const BookEdit = ({ book, onSubmitFromBookShow }) => {
+  const [title, setTitle] = useState(book.title);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmitFromBookShow(book.id, title);
+  };
+
+  return (
+    <form className="book-edit" onSubmit={handleSubmit}>
+      <label>title</label>
+      <input
+        className="input"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button className="button is-primary">save</button>
+    </form>
+  );
 };
 
 export default BookEdit;
